85cda097e81392b6c976fc8c5b29f6deafb764be
notasquid1938 <benshoemaker15@gmail.com>
2025-04-25 01:01:49 -0500

    Added inputs and outputs to microblaze for generation

--- sources_1/imports/lab7_1_provided_fa24/mb_usb_hdmi_top.sv
+++ sources_1/imports/lab7_1_provided_fa24/mb_usb_hdmi_top.sv
@@ -4,6 +4,14 @@ module final_project(
     input logic Clk,
     input logic reset_rtl_0,
     
+    //USB signals
+    input logic [0:0] gpio_usb_int_tri_i,
+    output logic gpio_usb_rst_tri_o,
+    input logic usb_spi_miso,
+    output logic usb_spi_mosi,
+    output logic usb_spi_sclk,
+    output logic usb_spi_ss,
+    
     //UART
     input logic uart_rtl_0_rxd,
     output logic uart_rtl_0_txd,
@@ -12,12 +20,18 @@ module final_project(
     output logic hdmi_tmds_clk_n,
     output logic hdmi_tmds_clk_p,
     output logic [2:0]hdmi_tmds_data_n,
-    output logic [2:0]hdmi_tmds_data_p
+    output logic [2:0]hdmi_tmds_data_p,
+    
+    //HEX displays
+    output logic [7:0] hex_segA,
+    output logic [3:0] hex_gridA,
+    output logic [7:0] hex_segB,
+    output logic [3:0] hex_gridB
     );
     
     assign reset_ah = reset_rtl_0;
     
-    mb_block lab_7_1_mb_(
+    mb_block_wrapper lab_7_1_mb_(
         .clk_100MHz(Clk),
         .reset_rtl_0(~reset_ah),
         .uart_rtl_0_rxd(uart_rtl_0_rxd), 
@@ -25,7 +39,45 @@ module final_project(
         .HDMI_0_tmds_clk_n(hdmi_tmds_clk_n),
         .HDMI_0_tmds_clk_p(hdmi_tmds_clk_p),
         .HDMI_0_tmds_data_n(hdmi_tmds_data_n),
-        .HDMI_0_tmds_data_p(hdmi_tmds_data_p)
+        .HDMI_0_tmds_data_p(hdmi_tmds_data_p),
+        
+        .gpio_usb_int_tri_i(gpio_usb_int_tri_i),
+        .gpio_usb_keycode_0_tri_o(keycode0_gpio),
+        .gpio_usb_keycode_1_tri_o(keycode1_gpio),
+        .gpio_usb_rst_tri_o(gpio_usb_rst_tri_o),
+        .usb_spi_miso(usb_spi_miso),
+        .usb_spi_mosi(usb_spi_mosi),
+        .usb_spi_sclk(usb_spi_sclk),
+        .usb_spi_ss(usb_spi_ss),
+        .gpio_execute_tri_o(execute),
+        .gpio_generated_ascii_tri_i(generated_ascii),
+        .gpio_generate_count_tri_i(generate_count)
+    );
+
+    logic [31:0] keycode0_gpio, keycode1_gpio;
+
+    logic reset_ah;
+    logic execute;
+    logic [7:0] generated_ascii;
+    logic [11:0] generate_count;
+    assign reset_ah = reset_rtl_0;
+    
+    
+   //Keycode HEX drivers
+    hex_driver HexA (
+        .clk(Clk),
+        .reset(reset_ah),
+        .in({keycode0_gpio[31:28], keycode0_gpio[27:24], keycode0_gpio[23:20], keycode0_gpio[19:16]}),
+        .hex_seg(hex_segA),
+        .hex_grid(hex_gridA)
+    );
+    
+    hex_driver HexB (
+        .clk(Clk),
+        .reset(reset_ah),
+        .in({keycode0_gpio[15:12], keycode0_gpio[11:8], keycode0_gpio[7:4], keycode0_gpio[3:0]}),
+        .hex_seg(hex_segB),
+        .hex_grid(hex_gridB)
     );
     
     
---
+++ sources_1/new/test_ascii_generate.sv
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 04/25/2025 01:00:29 AM
+// Design Name: 
+// Module Name: test_ascii_generate
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module test_ascii_generate(
+
+    );
+endmodule
