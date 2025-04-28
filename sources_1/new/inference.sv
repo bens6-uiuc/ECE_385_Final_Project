10fdfd16c1bb8f1fa486e54046cb953a87c34675
notasquid1938 <benshoemaker15@gmail.com>
2025-04-27 23:58:23 -0500

    Began working on inference module

--- sources_1/new/inference.sv
+++ sources_1/new/inference.sv
@@ -21,6 +21,51 @@
 
 
 module inference(
-
+    input logic clk,
+    input logic [7:0] input_ascii,
+    
+    output logic [7:0] generated_ascii,
+    output logic [11:0] generate_count,
+    output logic generate_complete
     );
+    
+    logic [7:0] generated_ascii;
+    logic [11:0] generate_count;
+    logic [6:0]token;
+    
+    logic [31:0] hidden_layer[16];
+    
+    ascii_to_token (
+        .input_ascii(input_ascii),
+        .output_token(token)
+    );
+    
+    floating_point_adder adder1 (
+  .aclk(clk),                                  // input wire aclk
+  .s_axis_a_tvalid(s_axis_a_tvalid),            // input wire s_axis_a_tvalid
+  .s_axis_a_tready(s_axis_a_tready),            // output wire s_axis_a_tready
+  .s_axis_a_tdata(s_axis_a_tdata),              // input wire [31 : 0] s_axis_a_tdata
+  .s_axis_b_tvalid(s_axis_b_tvalid),            // input wire s_axis_b_tvalid
+  .s_axis_b_tready(s_axis_b_tready),            // output wire s_axis_b_tready
+  .s_axis_b_tdata(s_axis_b_tdata),              // input wire [31 : 0] s_axis_b_tdata
+  .m_axis_result_tvalid(m_axis_result_tvalid),  // output wire m_axis_result_tvalid
+  .m_axis_result_tready(m_axis_result_tready),  // input wire m_axis_result_tready
+  .m_axis_result_tdata(m_axis_result_tdata)    // output wire [31 : 0] m_axis_result_tdata
+);
+
+    floating_point_multiply multiply1 (
+  .aclk(clk),                                  // input wire aclk
+  .s_axis_a_tvalid(s_axis_a_tvalid),            // input wire s_axis_a_tvalid
+  .s_axis_a_tready(s_axis_a_tready),            // output wire s_axis_a_tready
+  .s_axis_a_tdata(s_axis_a_tdata),              // input wire [31 : 0] s_axis_a_tdata
+  .s_axis_b_tvalid(s_axis_b_tvalid),            // input wire s_axis_b_tvalid
+  .s_axis_b_tready(s_axis_b_tready),            // output wire s_axis_b_tready
+  .s_axis_b_tdata(s_axis_b_tdata),              // input wire [31 : 0] s_axis_b_tdata
+  .m_axis_result_tvalid(m_axis_result_tvalid),  // output wire m_axis_result_tvalid
+  .m_axis_result_tready(m_axis_result_tready),  // input wire m_axis_result_tready
+  .m_axis_result_tdata(m_axis_result_tdata)    // output wire [31 : 0] m_axis_result_tdata
+);
+
+
+    
 endmodule
